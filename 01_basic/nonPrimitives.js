142ad9d39e5cdfe3e8edf917382ad6f13beb80e5
Ankitraj5614 <119001934+Ankitraj5614@users.noreply.github.com>
2025-01-11 21:12:29 +0530

    Add files via upload

---
+++ 01_basic/nonPrimitives.js
@@ -0,0 +1,25 @@
+let username = {
+    firstname : "Ankit",
+    isLoggedin : true, 
+};
+// username.firstnamen = "Mr. H";
+uasername.lastname = "raj"
+console.log(username.lastname);
+
+// console.log(username.firstname);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+//console.log(today.getDateL());
+ 
+// Array 
+let anotherUser = ["hitesh", true];
+
+console.log(anotehrUser[0]);
+// console.log("1" + 1) //ans = 11
+
+// let isValue = "2abc";
+// console.log(typeof Number(isValue));
+// // console.assert(Number(null));
---
+++ 01_basic/primitives.js
@@ -0,0 +1,42 @@
+let balance =120
+let anotherBalance = new Number(120)
+
+// console.log(balance);
+// console.log(anotherBalance.valueOf())
+
+
+// console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+// Boolean
+
+let isActive = true;
+let isReallyActive = new Boolean(true); //not recommended
+
+//  null and undefined 
+
+let firstname = null;
+let lastname = undefined;
+
+// console.log(firstname);
+// console.log(lastname);
+
+//sting
+ 
+let myString = "hello";
+let myStringOne = 'hola';
+let username = 'ankit';
+ 
+let oldGreat = myString + " " + "ankit";
+console.log(oldGreat);
+
+let greetMesssage = `Hello ${username} !`;
+let demoOne = `Value is ${2 * 2}`;
+console.log(demoOne);
+
+let sm1 = Symbol("ankit");
+let sm2 = Symbol("ankit");
+
+// console.log(sm1 == sm2); // false
+console.log(sm1); 
+
